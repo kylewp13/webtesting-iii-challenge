8a97d640f3894315b4a03861d259788f07a19d3e
Kyle Porter <kyleporter@Kyles-MacBook-Air.local>
2019-12-17 06:59:30 -0500

    added test for renders, unlocked and open, and snapshot

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+import React from "react";
+import { render } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+
+test("Dashboard renders", () => {
+  render(<Dashboard />);
+});
+
+test("Gate default to unlocked and open", () => {
+  const { getByText } = render(<Dashboard />);
+  expect(getByText(/open/i));
+  expect(getByText(/unlocked/i));
+});
+
+test("Display should match snapshot", () => {
+  expect(render(<Dashboard />)).toMatchSnapshot();
+});
