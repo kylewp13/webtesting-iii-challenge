877123d7f82359da2202eeecd7b1de68bee012fe
Kyle Porter <kyleporter@Kyles-MacBook-Air.local>
2019-12-17 07:05:11 -0500

    added test for display locked, class if locked, class if unlocked

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,28 @@
 // Test away!
+import React from "react";
+import { render, queryByText } from "@testing-library/react";
+import Display from "./Display";
+
+test("Display renders", () => {
+  render(<Display />);
+});
+
+test("Display - Closed if the closed prop is true", () => {
+    const { getByText, queryByText } = render(<Display closed={true} />);
+    expect(getByText(/closed/i));
+});
+
+test("Display Locked", () => {
+  const { getByText, queryByText } = render(<Display locked={false} />);
+  expect(getByText(/unlocked/i));
+});
+
+test("Class - red-led if Locked", () => {
+  const { getByText } = render(<Display locked={true} />);
+  expect(getByText(/locked/i).classList.contains("red-led")).toBe(true);
+});
+
+test("Class - green-led if Unlocked or Open", () => {
+    const { getByText, queryByText } = render(<Display locked={false} />);
+    expect(getByText(/unlocked/i).classList.contains("green-led")).toBe(true);
+});
